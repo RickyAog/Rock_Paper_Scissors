d8e56fcc99e052c19f4857b8e974f8ff28eeb830
Ricky <richard.anderson.2021@student.lhschools.org>
2019-05-22 13:40:42 -0400

    switched to using numbers

--- src/Rock_Paper_Scissors.java
+++ src/Rock_Paper_Scissors.java
@@ -4,36 +4,30 @@ import java.util.Scanner;
 public class Rock_Paper_Scissors {
     public static void main(String[] args) {
         System.out.println("________________________________________");
-        System.out.println("|Welcome Roshambo                      |");
-        System.out.println("|Ro is Rock Sham is paper Bo is scissor|");
+        System.out.println("|Welcome Ro-sham-bo                    |");
+        System.out.println("|0 is Rock, 1 is paper, 2 is scissor   |");
         System.out.println("|Type what you would like to play      |");
         System.out.println("________________________________________");
 
         Scanner keyboard = new Scanner(System.in);
-        for (int rsbCPU=rsbCPU();rsbCPU<7;rsbCPU++) {
-            rsbPlayer(keyboard);
-            rsbCPU();
-            rsbTie(keyboard);
-            rsbCPUWinner(keyboard);
-            rsbPlayerWinner(keyboard);
+        for (int rsbPlayer=rsbPlayer(keyboard);rsbPlayer<5;rsbPlayer++) {
+            startGame(keyboard);
         }
     }
 
 
+
     public static int rsbPlayer(Scanner keyboard) {
-        int Ro =0;
-        int Sham=1;
-        int bo=2;
-        String rsbPlayer;
-        rsbPlayer = keyboard.nextLine().toUpperCase();
-        if (rsbPlayer == "RO") {
-            System.out.println("You pick RO");
+        int rsbPlayer;
+        rsbPlayer = Integer.parseInt(keyboard.nextLine().toUpperCase());
+        if (rsbPlayer == 0) {
+            System.out.println("You pick ROCK");
         }
-        if (rsbPlayer == "SHAM") {
-            System.out.println("You pick SHAM");
+        if (rsbPlayer == 1) {
+            System.out.println("You pick PAPER");
         }
-        if (rsbPlayer == "BO") {
-            System.out.println("You pick BO");
+        if (rsbPlayer == 2) {
+            System.out.println("You pick SCISSOR");
         }
         return 2;
     }
@@ -42,13 +36,13 @@ public class Rock_Paper_Scissors {
     public static int rsbCPU() {
         int rsbCPU = (int) (Math.random() * 100 / 50);
         if (rsbCPU == 0) {
-            System.out.println("The CPU picked RO");
+            System.out.println("The CPU picked ROCK");
         }
         if (rsbCPU == 1) {
-            System.out.println("The CPU picked SHAM");
+            System.out.println("The CPU picked PAPER");
         }
         if (rsbCPU == 2) {
-            System.out.println("The CPU picked SHAM");
+            System.out.println("The CPU picked SCISSOR");
             }
 
 
@@ -58,27 +52,76 @@ public class Rock_Paper_Scissors {
     public static int rsbTie(Scanner keyboard){
         if (rsbPlayer(keyboard) == rsbCPU()) {
         System.out.println("You tied");
-
+System.out.println("+0 point");
         }
         return 2;
     }
     public static int rsbPlayerWinner(Scanner keyboard) {
 
-        //RSBPLAYER WINS
-        if (rsbPlayer(keyboard) > rsbCPU()) {
+        if (rsbPlayer(keyboard) != rsbCPU()) {
             System.out.println("You win");
-
+System.out.println("+ 1 for you");
         }
         return 2;
     }
 
     public static int rsbCPUWinner(Scanner keyboard) {
-        if (rsbCPU() > rsbPlayer(keyboard)) {
+        if (rsbCPU() != rsbPlayer(keyboard)) {
             System.out.println("You lose");
-
+System.out.println("+ 1 point for the cpu");
         }
 
         return 2;
 
     }
+    public static int startGame(Scanner keyboard) {
+        rsbPlayer(keyboard);
+        rsbCPU();
+        if (rsbPlayer(keyboard) == rsbCPU()) {
+            rsbTie(keyboard);
+        }
+        if (rsbPlayer(keyboard) == 0) if (rsbCPU() == 2) {
+
+        }
+        {
+            rsbPlayerWinner(keyboard);
+        }
+        if (rsbPlayer(keyboard) == 1) if (rsbCPU() == 0) {
+
+        }
+        {
+            rsbPlayerWinner(keyboard);
+        }
+        if (rsbCPU() == 2) if (rsbPlayer(keyboard) == 1) {
+
+        }
+        {
+            rsbCPUWinner(keyboard);
+        }
+        if (rsbCPU() == 0) if (rsbPlayer(keyboard) == 2) {
+
+        }
+        {
+            rsbCPUWinner(keyboard);
+        }
+        if (rsbCPU() == 1) if (rsbPlayer(keyboard) == 0) {
+
+        }
+        {
+            rsbCPUWinner(keyboard);
+        }
+        if (rsbCPU() == 2) if (rsbPlayer(keyboard) == 1) {
+
+        }
+        {
+            rsbCPUWinner(keyboard);
+        }
+
+
+
+
+
+    return 0;
+    }
+
 }
